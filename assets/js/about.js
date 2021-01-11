a8700176b839d89e2910d7489995e6f7ebf17761
Hanan Mifah <se.hanan.miftah@gmail.com>
2021-01-11 10:40:31 -0800

    The main changes in this commit are the following -->I have created files in the neccessary folders, for example an html file named about.html for about page, a css file inside css folder which is found in assets folder for styling the about page and named about.css and javascript file as well inside the js folder which is also found in assets folder. --> I have also added some images in images folder also renamed some image names --**--**-- major changes in about page are as follows --**--**--  -->I have used diferent layout methods like grid and flex  -->sticky element in the middile of the page  -->contact form  -->image modal using javascript and bootstrap classes  -->making the same header and footer as index.html file for consistency

---
+++ assets/js/about.js
@@ -0,0 +1,33 @@
+function openModal(){
+    document.getElementById('my-modal').style.display = "inline-flex";
+}
+
+function closeModal(){
+    document.getElementById('my-modal').style.display = "none";
+}
+
+var slideIndexJs = 1;
+
+showSlides(slideIndexJs);
+
+function changeSlides(n){
+    showSlides(slideIndexJs += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndexJs = n);
+}
+
+function showSlides(n){
+    var i;
+    var slidesJS = document.getElementsByClassName("my-slides");
+    if (n > slidesJS.length){
+        slideIndexJs = 1
+    }if (n < 1){ slideIndexJs = slidesJS.length;}
+
+    for(i = 0; i < slidesJS.length; i++){
+        slidesJS[i].style.display = "none";
+    }
+
+    slidesJS[slideIndexJs-1].style.display = "block";
+}
